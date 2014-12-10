6d0085be7449c26b17bd23727271e61d72230593
Jan-Philipp Steinwender <steinwenderjan@hotmail.com>
2014-12-10 08:51:58 +0100

    erstellung der datei

---
+++ LightOutPanel.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package weber_straeszler_steinwender_A08;
+
+import javax.swing.JPanel;
+
+/**
+ * View Klasse des Spieles Lights Out
+ * @author Jan-Philipp Steinwender
+ * @version 10.12.2014
+ */
+public class LightOutPanel extends JPanel{
+
+	
+	public LightOutPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
