86f5ca706cdf6417ba868ca3ec22a11c5475408f
Jan-Philipp Steinwender <steinwenderjan@hotmail.com>
2014-12-10 09:28:39 +0100

    paar sachen hinzugefügt für die panel und frame klassen

--- LightsOutController.java
+++ LightsOutController.java
@@ -6,11 +6,25 @@ package weber_straeszler_steinwender_A08;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+
 /**
- * @author Martin
+ * @author Martin, Jan-Philipp Steinwender
  *
  */
 public class LightsOutController implements ActionListener {
+	
+	private LightsOutPanel panel;
+	private LightsOutFrame frame;
+	
+	/**
+	 * Standardkonstruktor
+	 */
+	public LightsOutController(){
+		panel = new LightsOutPanel(this);
+		frame = new LightsOutFrame("Lights Out Game", panel);
+	}
+	
 	/**
 	 * 
 	 */
@@ -20,6 +34,6 @@ public class LightsOutController implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-
+		new LightsOutController();
 	}
 }
