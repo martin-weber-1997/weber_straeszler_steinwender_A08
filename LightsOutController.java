a43f2d51ee6b71560474954bfb85fee52f6a1d1d
Martin Weber <mweber02@student.tgm.ac.at>
2014-12-10 08:48:00 +0100

    Readme hinzugefuegt

---
+++ LightsOutController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package weber_straeszler_steinwender_A08;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Martin
+ *
+ */
+public class LightsOutController implements ActionListener {
+	/**
+	 * 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
