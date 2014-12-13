a2efa0e9f7f88733fa277d39a9e9cceea9b46243
Lukas Straessler <lukas.straessler@gmx.at>
2014-12-13 15:39:23 +0100

    Methoden initalisiert

--- LightsOutModel.java
+++ LightsOutModel.java
@@ -1,4 +1,9 @@
 package weber_straeszler_steinwender_A08;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
 /***
  * Model-Klasse des Spieles "Lights Out" 
  * @author Lukas Straessler
@@ -6,13 +11,21 @@ package weber_straeszler_steinwender_A08;
  */
 public class LightsOutModel {
 	
-	private boolean[][] field;
+	private byte[][] field;
+	private LightsOutPanel p;
 	
 	/**
 	 * Konstruktor
 	 */
-	public LightsOutModel(){
+	public LightsOutModel(LightsOutPanel p){
+		field = new byte[5][5];
+		this.p = p;
 		
+		for(int i = 0; i<5; i++){
+			for(int j = 0; j<5; j++){
+				field[i][j] = 0;
+			}
+		}
 	}
 	
 	/**
@@ -20,16 +33,22 @@ public class LightsOutModel {
 	 * @param row
 	 * @param column
 	 */
-	private void firstRandomLight(int row, int column){
-		
+	private void firstRandomLight(){
+		int firstLight1 = (int)Math.random() * (25 - 1) + 1;
+		int firstLight2 = (int)Math.random() * (25 - 1) + 1;
+		p.getButtons()[firstLight1][firstLight2].setBackground(Color.WHITE);
 	}
 	
 	/**
 	 * Kontrolliert ob das jeweilige Licht leuchtet
-	 * @return
+	 * @return boolean light
 	 */
 	private boolean light(int row, int column){
-		return true;
+		boolean light = false;
+		if(p.getButtons()[row][column].getBackground() == Color.WHITE){
+			light = true;
+		}
+		return light;
 	}
 	
 	/**
@@ -39,15 +58,44 @@ public class LightsOutModel {
 	 * @param column
 	 */
 	private void makeAMove(int row, int column){
-		
+		if(light(row,column) == true){
+			
+		}else{
+			p.getButtons()[row][column].setBackground(Color.WHITE);
+			p.getButtons()[row+1][column].setBackground(Color.BLACK);
+			p.getButtons()[row-1][column].setBackground(Color.BLACK);
+			p.getButtons()[row][column+1].setBackground(Color.BLACK);
+			p.getButtons()[row+1][column-1].setBackground(Color.BLACK);
+		}
 	}
 	
 	/**
 	 * Ueberprueft ob alle Lichter aus sind
-	 * @return
+	 * @return boolean
 	 */
 	private boolean hasWon(){
+		for(int i = 0; i<5; i++){
+			for(int j = 0; j<5; j++){
+				if(p.getButtons()[i][j].getBackground() == Color.BLACK){
+					
+				}else{
+					return false;
+				}
+			}
+		}
 		return true;
+	}
+	
+	/**
+	 * Setzt den Button-Array wieder zurück
+	 */
+	private void newGame(){
+		for(int i = 0; i<5; i++){
+			for(int j = 0; j<5; j++){
+				p.getButtons()[i][j].setBackground(Color.BLACK);
+			}
+		}
+		firstRandomLight();
 	}
 
 }
