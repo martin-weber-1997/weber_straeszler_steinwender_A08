eba9213a78e421cd8c3894eb80e528f1c89239e0
Martin Weber <mweber02@student.tgm.ac.at>
2014-12-17 09:09:52 +0100

    improved makeAMove Method

--- LightsOutModel.java
+++ LightsOutModel.java
@@ -6,194 +6,97 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 
 /***
- * Model-Klasse des Spieles "Lights Out" 
+ * Model-Klasse des Spieles "Lights Out"
+ * 
  * @author Lukas Straessler
  * @version 10.12.2014
  */
 public class LightsOutModel {
-	
-	private byte[][] field;
-	
+
+	private boolean[][] field;
+	private final int MAX_SIZE = 5;
+
 	/**
 	 * Konstruktor
 	 */
-	public LightsOutModel(){
-		field = new byte[5][5];
-		
-		for(int i = 0; i<5; i++){
-			for(int j = 0; j<5; j++){
-				field[i][j] = 0;
-			}
-		}
-	}
-	
-	/**
-	 * Das Anfangslicht das am Anfang des Spieles leuchtet
-	 * @param row
-	 * @param column
-	 */
-	private int[] firstRandomLight(){
-		int lights = (int)Math.random() * (5 - 1) + 1;
-		ArrayList<Integer> usedLights = new ArrayList();
-		usedLights.add(6);
-		int[] firstLights = new int[(lights*2) -1];
-		int i = 0;
-		
-		while(i<lights){
-			int firstLight1 = (int)Math.random() * (25 - 1) + 1;
-			int firstLight2 = (int)Math.random() * (25 - 1) + 1;
-			boolean isAllreadyUsed = false;
-			for(int j = 0; j<usedLights.size(); j++){
-				if(firstLight1 == usedLights.get(j) || firstLight2 == usedLights.get(j)){//Ueberpruefung ob die Lichter schon leuchten
-					isAllreadyUsed = true;
-					break;
-				}
-			}
-			
-			if(isAllreadyUsed != true){//Wenn sie nocht nicht im Array stehen, werden sie hier ihm hinzugefuegt und der usedLights-List hinzugefuegt, damit sie spaeter nicht nochmal hinzugefuegt werden
-				firstLights[i] = firstLight1;
-				firstLights[i+1] = firstLight2;
-				usedLights.add(firstLight1);
-				usedLights.add(firstLight2);
-				i++;
-			}
-			
-		}
-		return firstLights;
+	public LightsOutModel() {
+		field = new boolean[MAX_SIZE][MAX_SIZE];
+		init();
 	}
 
 	/**
-	 * Ueberpruefung ob der Zug moeglich ist
-	 * Wenn ja, dann wird ein Integer-Array mit den zu aendernden Buttons zurueck gegeben
-	 * @param z
-	 * @return JButton
+	 * 
 	 */
-	private int[] makeAMove(int z){
-		String button = Integer.toString(z);
-		String[] b = button.split("/");
-		int row = Integer.parseInt(b[0]);
-		int column = Integer.parseInt(b[1]);
-		int[] buttonToChange;
-		
-		if(row == 2 || row == 3 || row == 4 && column == 1){
-			buttonToChange = new int[8];
-			buttonToChange[0] = row+1;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row-1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column+1;
-			buttonToChange[6] = row;
-			buttonToChange[7] = column;
-		}else if(row == 2 || row == 3 || row == 4 && column == 5){
-			buttonToChange = new int[8];
-			buttonToChange[0] = row+1;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row-1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column-1;
-			buttonToChange[6] = row;
-			buttonToChange[7] = column;
-		}else if(column == 2 || column == 3 || column == 4 && row == 1){
-			buttonToChange = new int[8];
-			buttonToChange[0] = row+1;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column+1;
-			buttonToChange[6] = row;
-			buttonToChange[7] = column-1;
-		}else if(column == 2 || column == 3 || column == 4 && row == 5){
-			buttonToChange = new int[8];
-			buttonToChange[0] = row;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row-1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column+1;
-			buttonToChange[6] = row;
-			buttonToChange[7] = column-1;
-		}else if(row == 1 && column == 1){
-			buttonToChange = new int[6];
-			buttonToChange[0] = row;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row+1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column+1;
-		}else if(row == 5 && column == 1){
-			buttonToChange = new int[6];
-			buttonToChange[0] = row;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row-1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column+1;
-		}else if(row == 1 && column == 5){
-			buttonToChange = new int[6];
-			buttonToChange[0] = row;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row+1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column-1;
-		}else if(row == 5 && column == 5){
-			buttonToChange = new int[6];
-			buttonToChange[0] = row;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row-1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row;
-			buttonToChange[5] = column-1;
-		}else{
-			buttonToChange = new int[10];
-			buttonToChange[0] = row;
-			buttonToChange[1] = column;
-			buttonToChange[2] = row+1;
-			buttonToChange[3] = column;
-			buttonToChange[4] = row-1;
-			buttonToChange[5] = column;
-			buttonToChange[6] = row;
-			buttonToChange[7] = column+1;
-			buttonToChange[8] = row;
-			buttonToChange[9] = column-1;
+	public void init() {
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				field[i][j] = false;
+			}
 		}
-		
-		
-		
-		return buttonToChange;
+		firstRandomLight();
 	}
-	
-//	/**
-//	 * Ueberprueft ob alle Lichter aus sind
-//	 * @return boolean
-//	 */
-//	private boolean hasWon(){
-//		for(int i = 0; i<5; i++){
-//			for(int j = 0; j<5; j++){
-//				if(p.getButtons()[i][j].getBackground() == Color.BLACK){
-//					
-//				}else{
-//					return false;
-//				}
-//			}
-//		}
-//		return true;
-//	}
-	
-//	/**
-//	 * Setzt den Button-Array wieder zurueck
-//	 * In Controller
-//	 */
-//	private void newGame(){
-//		for(int i = 0; i<5; i++){
-//			for(int j = 0; j<5; j++){
-//				p.getButtons()[i][j].setBackground(Color.BLACK);
-//			}
-//		}
-//		firstRandomLight();
-//	}
+
+	/**
+	 * Die Anfangslichter die am Anfang des Spieles leuchten Das Spiel ist immer
+	 * lösbar
+	 * 
+	 */
+	private void firstRandomLight() {
+		int press = (int) (Math.random() * 50);
+		for (int i = 0; i < press; i++) {
+			int row = (int) (Math.random() * MAX_SIZE);
+			int column = (int) (Math.random() * MAX_SIZE);
+			makeAMove(row, column);
+		}
+
+	}
+
+	/**
+	 * 
+	 * 
+	 */
+	private boolean[][] makeAMove(int row, int column) {
+
+		if (column != 0)
+			field[row][column - 1] = !field[row][column - 1];
+		if (column != MAX_SIZE)
+			field[row][column + 1] = !field[row][column + 1];
+		if (row != 0)
+			field[row - 1][column] = !field[row - 1][column];
+		if (row != MAX_SIZE)
+			field[row + 1][column] = !field[row + 1][column];
+
+		return field;
+	}
+
+	// /**
+	// * Ueberprueft ob alle Lichter aus sind
+	// * @return boolean
+	// */
+	// private boolean hasWon(){
+	// for(int i = 0; i<5; i++){
+	// for(int j = 0; j<5; j++){
+	// if(p.getButtons()[i][j].getBackground() == Color.BLACK){
+	//
+	// }else{
+	// return false;
+	// }
+	// }
+	// }
+	// return true;
+	// }
+
+	// /**
+	// * Setzt den Button-Array wieder zurueck
+	// * In Controller
+	// */
+	// private void newGame(){
+	// for(int i = 0; i<5; i++){
+	// for(int j = 0; j<5; j++){
+	// p.getButtons()[i][j].setBackground(Color.BLACK);
+	// }
+	// }
+	// firstRandomLight();
+	// }
 
 }
